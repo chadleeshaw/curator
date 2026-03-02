910b837aa348222445626aa4a04b4329dd7d6ba4
Chad Shaw <10213220+chadleeshaw@users.noreply.github.com>
2026-03-01 21:51:36 -0700

    Shorten card issue count labels to prevent text wrapping (#25)

--- static/js/features/tracking.js
+++ static/js/features/tracking.js
@@ -1071,7 +1071,7 @@ export class TrackingManager {
       : '';
     const issueStats =
       totalKnown > 0 ? `<span class="issue-count">${totalKnown} issues found</span>` : '';
-    const libraryStats = `<span class="library-count"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" style="vertical-align:-1px"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg> ${libraryCount} in library</span>`;
+    const libraryStats = `<span class="library-count"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" style="vertical-align:-1px"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg> ${libraryCount} issues</span>`;
     const selectedStats =
       selectedCount > 0
         ? `<span class="selected-count">\u2022 ${selectedCount} selected</span>`
